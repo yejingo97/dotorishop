2cf7f0d183beb2b61d7a6485368b939722e62122
go <go@gmail.com>
2024-04-20 14:28:00 +0900

    revers01

--- src/Components/Home/HomeReview.js
+++ src/Components/Home/HomeReview.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Style from './css/HomeReview.module.css'
+import { FaStar } from "react-icons/fa";
 
 export default function HomeReview() {
   return (
@@ -11,18 +12,62 @@ export default function HomeReview() {
             <div className={Style.review_list_wrap}>
                 <ul className={Style.review_list}>
                     <li>
-                        <p className={Style.review_products_img}>
-
-                        </p>
                         <p className={Style.review_products_name}>
                             [토미카]드림토키마 지브리 09...
                         </p>
+                        <div className={Style.review_products_img}>
+
+                        </div>
                         <div className={Style.review_intro}>
-                            <p className={Style.review_img}>
-
-                            </p>
                             <p className={Style.review_star}>
+                                <FaStar/>
+                                <FaStar/>
+                                <FaStar/>
+                                <FaStar/>
+                                <FaStar/>
+                                5
+                            </p>
+                            <p className={Style.review_txt}>
+                                너무너무 귀엽고 보들보들 좋습니다ㅠㅠㅠㅠㅠ
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <p className={Style.review_products_name}>
+                            [토미카]드림토키마 지브리 09...
+                        </p>
+                        <div className={Style.review_products_img}>
 
+                        </div>
+                        <div className={Style.review_intro}>
+                            <p className={Style.review_star}>
+                                <FaStar/>
+                                <FaStar/>
+                                <FaStar/>
+                                <FaStar/>
+                                <FaStar/>
+                                5
+                            </p>
+                            <p className={Style.review_txt}>
+                                너무너무 귀엽고 보들보들 좋습니다ㅠㅠㅠㅠㅠ
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <p className={Style.review_products_name}>
+                            [토미카]드림토키마 지브리 09...
+                        </p>
+                        <div className={Style.review_products_img}>
+
+                        </div>
+                        <div className={Style.review_intro}>
+                            <p className={Style.review_star}>
+                                <FaStar/>
+                                <FaStar/>
+                                <FaStar/>
+                                <FaStar/>
+                                <FaStar/>
+                                5
                             </p>
                             <p className={Style.review_txt}>
                                 너무너무 귀엽고 보들보들 좋습니다ㅠㅠㅠㅠㅠ
--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -4,10 +4,33 @@ import Style from './css/Footer.module.css'
 
 export default function Footer() {
   return (
-    <div className={Style.footer_wrap}>
-        <footer className={Style.footer}>
-            footer영역
-        </footer>
-    </div>
+    <footer className={Style.footer_wrap}>
+        <div className={Style.footer_top_wrap}>
+            <div className={Style.footer_top}>
+              <ul className={Style.footer_top_list}>
+                <li>회사소개</li>
+                <li>이용약관</li>
+                <li>개인정보처리방침</li>
+                <li>매장안내</li>
+                <li>고객센터</li>
+                <li>입점/제휴/광고안내</li>
+                <li>불법 라이센스 신고 및 제보</li>
+              </ul>
+              <div className={Style.footer_top_logo}>
+                <img src='../images/logo_img.png' alt='로고이미지'/>
+              </div>
+            </div>
+        </div>
+        <div className={Style.footer_bottom_wrap}>
+            <div className={Style.footer_bottom}>
+              <p>대원미디어(주)</p>
+              <p>서울특별시 용산구 한강대로23길 55 (용산역) 아이파크몰 6층 테마파크1-1 대원미디어(주)</p>
+              <p>통신판매신고번호 : 2007-서울용산-04838</p>
+              <p>대표자 : 정동훈</p>
+              <p>사업자등록번호 : 1068103487</p>
+              <p>개인정보 보호책임자 : 김기남 cs@daewonmedia.com</p>
+            </div>
+        </div>
+    </footer>
   )
 }
